4482d92f1e91248a9e542f1255c38516307b450f
Davidvera27 <davidrestrepovera@gmail.com>
2024-06-18 19:47:35 -0500

    primer tercero

--- backend/middleware/Auth.js
+++ backend/middleware/Auth.js
@@ -1,16 +1,14 @@
 // backend/middleware/auth.js
 const jwt = require('jsonwebtoken');
-const config = require('config');
 
 module.exports = function(req, res, next) {
   const token = req.header('x-auth-token');
-
   if (!token) {
     return res.status(401).json({ msg: 'No token, authorization denied' });
   }
 
   try {
-    const decoded = jwt.verify(token, config.get('JWT_SECRET'));
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded.user;
     next();
   } catch (err) {
