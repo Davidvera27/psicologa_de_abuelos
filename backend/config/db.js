3663c5333598fa79688649a8c8c1ac66db2b4aab
Davidvera27 <davidrestrepovera@gmail.com>
2024-06-19 00:27:14 -0500

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,16 @@
+//backend\config\db.js
 const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('MONGO_URI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
     });
-    console.log('MongoDB Connected...');
-  } catch (err) {
-    console.error(err.message);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
     process.exit(1);
   }
 };
