a1a9920fcb7a8c25ebd2aa831f7aceb9a094244c
Davidvera27 <davidrestrepovera@gmail.com>
2024-07-22 23:21:26 -0500

    Create index.js

---
+++ frontend/src/theme/index.js
@@ -0,0 +1,18 @@
+// src/theme/index.js
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#3f51b5',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+    background: {
+      default: '#E0F7FA',
+    },
+  },
+});
+
+export default theme;
