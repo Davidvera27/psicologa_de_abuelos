45f97b1ef62b10da97f39d3d7696c5fff61e2a16
Davidvera27 <davidrestrepovera@gmail.com>
2024-07-22 20:22:49 -0500

    Create Home.js

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,29 @@
+// Home.js
+import React from 'react';
+import styled from 'styled-components';
+import fondo from '../assets/lofo_psico_abuelos_full_hd.jpeg'; // Asegúrate de que la ruta sea correcta
+
+const HomeWrapper = styled.div`
+  height: 100vh;
+  background-image: url(${fondo});
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  text-align: center;
+  font-size: 2rem;
+`;
+
+function Home() {
+  return (
+    <HomeWrapper>
+      <div>
+        
+      </div>
+    </HomeWrapper>
+  );
+}
+
+export default Home;
