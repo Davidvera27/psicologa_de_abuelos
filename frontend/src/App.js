aac7e433bfb61075394e6b4d029086de16f0f4c0
Davidvera27 <davidrestrepovera@gmail.com>
2024-07-22 23:20:13 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,33 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
-import Pacientes from './pages/Pacientes';
-import AddPaciente from './pages/addPaciente';
-import PacienteDetalle from './pages/PacienteDetalle';
-import Conductor from './pages/conductor';
-import Cronograma from './pages/cronograma';
-import Evaluacion from './pages/Evaluacion';
-import Administrativo from './pages/Administrativo';
-import Historico from './pages/Historico';
-import Contrato1 from './components/Contrato1';
-import Contrato2 from './components/Contrato2'; // Importa el componente Contrato2
-import styled, { createGlobalStyle, ThemeProvider, keyframes } from 'styled-components';
-import { CssBaseline } from '@mui/material';
-import theme from './theme';
-import Breadcrumbs from './components/Breadcrumbs';
-import backgroundImage from './assets/lofo_psico_abuelos_full_hd.jpeg';
-import NavBar from './components/NavBar';
-import Login from './pages/login';
-import PeopleIcon from '@mui/icons-material/People';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import DirectionsBusIcon from '@mui/icons-material/DirectionsBus';
-import ScheduleIcon from '@mui/icons-material/Schedule';
-import WorkIcon from '@mui/icons-material/Work';
-import AssessmentIcon from '@mui/icons-material/Assessment';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import Pacientes from "./pages/Pacientes";
+import AddPaciente from "./pages/addPaciente";
+import PacienteDetalle from "./pages/PacienteDetalle";
+import Conductor from "./pages/conductor";
+import Cronograma from "./pages/cronograma";
+import Evaluacion from "./pages/Evaluacion";
+import Administrativo from "./pages/Administrativo";
+import Historico from "./pages/Historico";
+import Contrato1 from "./components/Contrato1";
+import Contrato2 from "./components/Contrato2"; // Importa el componente Contrato2
+import styled, {
+  createGlobalStyle,
+  ThemeProvider,
+  keyframes,
+} from "styled-components";
+import { CssBaseline } from "@mui/material";
+import theme from "./theme";
+import Breadcrumbs from "./components/Breadcrumbs";
+import backgroundImage from "./assets/logo_psico_abuelos_full_hd.jpeg";
+import NavBar from "./components/NavBar";
+import Login from "./pages/login";
+import PeopleIcon from "@mui/icons-material/People";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import DirectionsBusIcon from "@mui/icons-material/DirectionsBus";
+import ScheduleIcon from "@mui/icons-material/Schedule";
+import WorkIcon from "@mui/icons-material/Work";
+import AssessmentIcon from "@mui/icons-material/Assessment";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -65,7 +69,7 @@ const CenterWrapper = styled.div`
 `;
 
 const CenterButton = styled(Link)`
-  color: ${props => props.theme.palette.text.secondary};
+  color: ${(props) => props.theme.palette.text.secondary};
   text-decoration: none;
   font-size: 1.2rem;
   font-weight: bold;
@@ -74,17 +78,27 @@ const CenterButton = styled(Link)`
   align-items: center;
   padding: 1.5rem;
   border-radius: 10px;
-  transition: background-color 0.3s ease, color 0.3s ease, box-shadow 0.3s ease, transform 0.3s ease;
-  background: linear-gradient(145deg, rgba(179, 157, 219, 1) 0%, rgba(103, 58, 183, 1) 100%);
-  border: 1px solid ${props => props.theme?.palette?.primary?.dark || '#673ab7'};
+  transition: background-color 0.3s ease, color 0.3s ease, box-shadow 0.3s ease,
+    transform 0.3s ease;
+  background: linear-gradient(
+    145deg,
+    rgba(179, 157, 219, 1) 0%,
+    rgba(103, 58, 183, 1) 100%
+  );
+  border: 1px solid
+    ${(props) => props.theme?.palette?.primary?.dark || "#673ab7"};
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08);
   animation: ${glow} 2s infinite alternate;
   &:hover {
-    background: linear-gradient(145deg, rgba(103, 58, 183, 1) 0%, rgba(179, 157, 219, 1) 100%);
+    background: linear-gradient(
+      145deg,
+      rgba(103, 58, 183, 1) 0%,
+      rgba(179, 157, 219, 1) 100%
+    );
     box-shadow: 0 10px 15px rgba(0, 0, 0, 0.2), 0 4px 6px rgba(0, 0, 0, 0.1);
     transform: translateY(-5px);
     svg {
-      color: ${props => props.theme?.palette?.text?.primary || '#fff'};
+      color: ${(props) => props.theme?.palette?.text?.primary || "#fff"};
       box-shadow: 0 0 10px rgba(255, 255, 255, 0.5);
       border-radius: 50px;
     }
@@ -113,8 +127,10 @@ const Spinner = styled.div`
   margin: 100px auto;
   width: 50px;
   height: 50px;
-  border: 8px solid ${props => props.theme?.palette?.primary?.light || '#b39ddb'};
-  border-top: 8px solid ${props => props.theme?.palette?.primary?.main || '#673ab7'};
+  border: 8px solid
+    ${(props) => props.theme?.palette?.primary?.light || "#b39ddb"};
+  border-top: 8px solid
+    ${(props) => props.theme?.palette?.primary?.main || "#673ab7"};
   border-radius: 50%;
   animation: ${spin} 1s linear infinite;
 `;
@@ -124,7 +140,11 @@ const LoadingContainer = styled.div`
   justify-content: center;
   align-items: center;
   height: 100vh;
-  background: linear-gradient(145deg, rgba(179, 157, 219, 1) 0%, rgba(103, 58, 183, 1) 100%);
+  background: linear-gradient(
+    145deg,
+    rgba(179, 157, 219, 1) 0%,
+    rgba(103, 58, 183, 1) 100%
+  );
 `;
 
 const LoadingText = styled.div`
@@ -139,8 +159,8 @@ const AuthenticatedApp = () => (
     <ContentWrapper>
       <Breadcrumbs />
       <Routes>
-        <Route 
-          path="/" 
+        <Route
+          path="/"
           element={
             <CenterWrapper>
               <CenterButton to="/pacientes">
@@ -180,7 +200,7 @@ const AuthenticatedApp = () => (
                 Administrativo
               </CenterButton>
             </CenterWrapper>
-          } 
+          }
         />
         <Route path="/pacientes" element={<Pacientes />} />
         <Route path="/add-paciente" element={<AddPaciente />} />
@@ -190,8 +210,10 @@ const AuthenticatedApp = () => (
         <Route path="/Evaluacion" element={<Evaluacion />} />
         <Route path="/Administrativo" element={<Administrativo />} />
         <Route path="/historico" element={<Historico />} /> {/* Nueva ruta */}
-        <Route path="/contrato1" element={<Contrato1 />} /> {/* Nueva ruta para Contrato1 */}
-        <Route path="/contrato2" element={<Contrato2 />} /> {/* Nueva ruta para Contrato2 */}
+        <Route path="/contrato1" element={<Contrato1 />} />{" "}
+        {/* Nueva ruta para Contrato1 */}
+        <Route path="/contrato2" element={<Contrato2 />} />{" "}
+        {/* Nueva ruta para Contrato2 */}
       </Routes>
     </ContentWrapper>
   </Router>
@@ -200,14 +222,15 @@ const AuthenticatedApp = () => (
 const App = () => {
   const { isLoading, error, isAuthenticated } = useAuth0();
 
-  if (isLoading) return (
-    <LoadingContainer>
-      <div>
-        <Spinner />
-        <LoadingText>Cargando...</LoadingText>
-      </div>
-    </LoadingContainer>
-  );
+  if (isLoading)
+    return (
+      <LoadingContainer>
+        <div>
+          <Spinner />
+          <LoadingText>Cargando...</LoadingText>
+        </div>
+      </LoadingContainer>
+    );
   if (error) return <div>Oops... {error.message}</div>;
   if (!isAuthenticated) return <Login />;
 
@@ -218,6 +241,6 @@ const App = () => {
       <AuthenticatedApp />
     </ThemeProvider>
   );
-}
+};
 
 export default App;
